e8413b6f41ca78269f63b57488bb4ba276f1f30e
Bare-Metal-Tom <tombarn65@gmail.com>
2025-06-14 20:35:10 -0400

    Add files via upload

--- BYPASS-LOCAL-AUTH/CLEAN-UP.bat
+++ BYPASS-LOCAL-AUTH/CLEAN-UP.bat
@@ -1,11 +1,6 @@
 @echo off
 cls
-if exist "C:\sethc_backup.exe" (
-    copy /Y "C:\sethc_backup.exe" "C:\Windows\System32\sethc.exe"
-) else (
-    echo Backup not found: C:\sethc_backup.exe
-    echo Nothing to restore.
-)
+copy /Y "C:\sethc_backup.exe" "C:\Windows\System32\sethc.exe"
 set /p choice=Press [E] to exit OR any other key to continue: 
 if /I "%choice%"=="E" (
     exit
